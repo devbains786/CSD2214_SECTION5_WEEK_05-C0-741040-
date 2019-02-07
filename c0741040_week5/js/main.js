1fe3fea804d461227acc610c01024d9a314a550f
visualstudio <visualstudio@230-PC45>
2019-02-07 11:43:27 -0500

    11:45 changes java script

--- c0741040_week5/js/main.js
+++ c0741040_week5/js/main.js
@@ -1,1 +1,12 @@
-﻿// Your code here!
+﻿function myFunction(a,b) {
+   
+    return (a + b);// The function returns 
+
+}
+function arrays() {
+
+    var pens;
+    pens = ["red", "orange", "green", "yellow", "blue"];
+    console.log(pens);
+
+}
